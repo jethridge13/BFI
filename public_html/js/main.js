51814cab6e565291ebcbdf40ffd3b4d6e9bb51c2
Joshua Ethridge <joshua.ethridge13@gmail.com>
2017-03-18 19:20:38 -0400

    CSS is annoying.

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,5 +1,5 @@
 var app = angular.module('BFI', []);
 
 app.controller('TapeCtrl', ['$scope', function ($scope) {
-        $scope.cells = new Array(15).fill(0);
+        $scope.cells = new Array(20).fill(0);
     }]);
