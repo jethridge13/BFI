c9e75a331b32cbd4f9758472feb88d74b7a41787
Joshua Ethridge <joshua.ethridge13@gmail.com>
2017-03-19 22:27:18 -0400

    More page formatting stuff

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,5 +1,9 @@
 var app = angular.module('BFI', []);
+var cells = 20;
+var arrowURL = "img/arrow.png";
+
+var cursor = 0;
 
 app.controller('TapeCtrl', ['$scope', function ($scope) {
-        $scope.cells = new Array(20).fill(0);
+        $scope.cells = new Array(cells).fill(0);
     }]);
